78a030b94666721916c25c457bd2f9ff336b3331
Vedant_pawar <pawarvedant1216@gmail.com>
2026-08-25 17:17:44 +0530

    Time: 3 ms (18.05%), Space: 47.2 MB (97.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,38 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int i=0;
+//         int j=i;
+         
+//          while(j<nums.length){
+//             if(nums[i]==0 && nums[j]!=0){
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 i++;
+//                 j++;
+//             }else if(nums[i]!=0){
+//                  i++;
+//                  j++;
+//             }else{
+//                 j++;
+//             }
+//          }
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
-        int j=i;
-         
-         while(j<nums.length){
-            if(nums[i]==0 && nums[j]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j++;
-            }else if(nums[i]!=0){
-                 i++;
-                 j++;
-            }else{
-                j++;
+
+        for(int j=0;j<nums.length;j++){
+            
+            if(nums[j]!=0){
+               int temp=nums[i];
+               nums[i]=nums[j];
+               nums[j]=temp;
+
+               i++;
             }
-         }
+        }
     }
 }
